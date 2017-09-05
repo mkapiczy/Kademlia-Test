af893cd2317cb7d3261fbfaa1b8f4874b8c3a52e
Michał Kapiczyński <mix.kap@gmail.com>
2017-09-05 15:52:00 +0200

    Basic REST API framework with use of express library

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-const http = require('http')  
-const port = 3000
+const express = require('express')  
+const app = express()  
+const dotenv = require('dotenv');
+var HttpStatus = require('http-status-codes');
 
-const requestHandler = (request, response) => {  
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
-}
+dotenv.load()
+const port = process.env.PORT
 
-const server = http.createServer(requestHandler)
+//Html Index
+app.get('/', (request, response) => {  
+  response.send('Hello from Express!')
+})
 
-server.listen(port, (err) => {  
+//PING
+app.get('/api/kademlia/ping', (request, response) => {    
+  response.status(HttpStatus.OK)
+  response.send("PONG")
+})
+
+//FIND_NODE
+app.get('/api/kademlia/find_node', (request, response) => {    
+  response.status(HttpStatus.OK)
+  response.setHeader('Content-Type', 'application/json')
+  //findClosestNodes
+  closestNodes = {}
+  response.json(JSON.stringify({closestNodes}))
+})
+
+app.listen(port, (err) => {  
   if (err) {
-    return console.log('something bad happened', err)
+    return console.log('Error: ', err)
   }
 
-  console.log(`server is listening on ${port}`)
+  console.log(`Server is listening on port ${port}`)
 })
