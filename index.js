256bc454e5cd51eedd5bec75f8b1de87e886d738
Michał Kapiczyński <mix.kap@gmail.com>
2017-09-05 16:10:27 +0200

    Adding test ping endpoint

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')  
+const request = require('request')
 const app = express()  
 const dotenv = require('dotenv');
 var HttpStatus = require('http-status-codes');
@@ -6,18 +7,20 @@ var HttpStatus = require('http-status-codes');
 dotenv.load()
 const port = process.env.PORT
 
+
 //Html Index
 app.get('/', (request, response) => {  
   response.send('Hello from Express!')
 })
 
-//PING
+//PING ENDPOINT
 app.get('/api/kademlia/ping', (request, response) => {    
   response.status(HttpStatus.OK)
   response.send("PONG")
 })
 
-//FIND_NODE
+
+//FIND_NODE ENDPOINT
 app.get('/api/kademlia/find_node', (request, response) => {    
   response.status(HttpStatus.OK)
   response.setHeader('Content-Type', 'application/json')
@@ -25,6 +28,25 @@ app.get('/api/kademlia/find_node', (request, response) => {
   closestNodes = {}
   response.json(JSON.stringify({closestNodes}))
 })
+
+//FIND_NODE ENDPOINT
+app.get('/test_ping', (request, response) => {    
+  sendPingRequest(0)
+})
+
+function sendPingRequest(nodeId){
+  //find node from buckets basedOn nodeId and based on that get request endpoint
+  nodeAddress = 'http://127.0.0.1:3000'
+  pingRequestEndpoint = nodeAddress + '/api/kademlia/ping'
+  request(pingRequestEndpoint, function(error, response, body){
+    if (error) {
+      console.log(error)
+    } else{
+      //addNode to the bucket
+      console.log(response.body)
+    }
+  })
+}
 
 app.listen(port, (err) => {  
   if (err) {
