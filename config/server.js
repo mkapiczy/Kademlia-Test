5b9f36bafe992a45f077d7eacd04a4e5401ab0e2
Lasse Have Nielsen <lhn@it-minds.dk>
2017-09-20 12:33:12 +0200

    Maybe the fix to the path.. No error :-)

--- config/server.js
+++ config/server.js
@@ -14,6 +14,10 @@ const app = express();
 
 const apiPath = "/api/kademlia/";
 
+//public purposes
+app.set("views", path.join(__dirname, ".././views/"));
+app.use("/views", express.static(path.join(__dirname, ".././views")));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "pug");
