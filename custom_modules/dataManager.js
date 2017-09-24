9290ceab20c1cd7222dd023d1b31545fb812d75c
Michał Kapiczyński <mix.kap@gmail.com>
2017-09-24 19:48:38 +0200

    [Bug fix] - Before getting value from the map we need to parse it to Number type.

--- custom_modules/dataManager.js
+++ custom_modules/dataManager.js
@@ -1,7 +1,6 @@
 const constants = require("./../config/constants");
 const util = require("./util");
 
-
 function DataManager() {
   this.dataStorage = new Map();
 }
@@ -12,17 +11,11 @@ DataManager.prototype.storeValue = function(key, value) {
 
 DataManager.prototype.storeValueWithKeyHashing = function(key, value) {
   hashedKey = util.createHashFromKey(key, constants.B / 8);
-
-  console.log("Name " + key);
-  console.log("Value " + value);
-  console.log("hashed key: " + hashedKey);
-
   this.dataStorage.set(hashedKey, value);
 };
 
 DataManager.prototype.findValue = function(key) {
-  value = this.dataStorage.get(key);
-  return value;
+  return this.dataStorage.get(Number(key));
 };
 
 module.exports = DataManager;
